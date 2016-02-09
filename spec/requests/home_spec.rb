88afd8a8962eb1a84451e2d36fb9f4de9ef89e5b
Benjamin Quorning <benjamin@quorning.net>
2016-02-09 17:40:46 +0100

    Simplify specs

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,34 +1,29 @@
 require 'spec_helper'
 
 describe "integration", type: :request do
-  describe 'index' do
-    it 'accepts :params' do
-      get "/home", params: { text: 'Hello World!' }
+  it 'accepts :params' do
+    get "/home", params: { text: 'Hello World!' }
 
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include('Text: Hello World!')
-    end
-
-  describe 'headers'
-    it 'accepts headers' do
-      get "/home", headers: { 'HTTP_ACCEPT' => 'text/plain' }
-
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include('Accept: text/plain')
-    end
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include('Text: Hello World!')
   end
 
-  describe 'raise on offense' do
-    it 'raises when an unexpected parameter is passed' do
-      expect {
-        get "/home", wrong: true
-      }.to raise_error(RailsTestParamsBackport::ParameterError)
-    end
+  it 'accepts headers' do
+    get "/home", headers: { 'HTTP_ACCEPT' => 'text/plain' }
 
-    it 'raises when headers are sent in a separate hash' do
-      expect {
-        get "/home", {}, { 'HTTP_ACCEPT' => 'text/plain' }
-      }.to raise_error(RailsTestParamsBackport::ParameterError)
-    end
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include('Accept: text/plain')
+  end
+
+  it 'raises when an unexpected parameter is passed' do
+    expect {
+      get "/home", wrong: true
+    }.to raise_error(RailsTestParamsBackport::ParameterError)
+  end
+
+  it 'raises when headers are sent in a separate hash' do
+    expect {
+      get "/home", {}, { 'HTTP_ACCEPT' => 'text/plain' }
+    }.to raise_error(RailsTestParamsBackport::ParameterError)
   end
 end
